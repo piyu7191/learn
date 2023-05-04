41a892be03ce906e1785b25b91bcb4513fdfd704
pallavi <pj4568@gmail.com>
2023-05-04 22:04:59 +0530

    first commit

---
+++ oops2.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def __init__(self):
+        self.fare = None
+    
+    def Fare(self, fare):
+        self.fare = fare
+
+bus, car, train, truck, ship = Vehicle(), Vehicle(), Vehicle(), Vehicle(), Vehicle()
+vehicles = [bus, car, train, truck, ship]
+bus.Fare(6)
+car.Fare(5)
+train.Fare(65)
+truck.Fare(50)
+ship.Fare(70)
+
+totalfare = sum([i.fare for i in vehicles])
+print(totalfare)
